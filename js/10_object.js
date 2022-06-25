cd31d0c9c0aacc199d9ed73b41a7585284ba8c50
Taku0813 <taku08132001@icloud.com>
2022-06-25 21:18:30 +0900

    11まで終わり

---
+++ js/10_object.js
@@ -0,0 +1,5 @@
+const personalData ={ name :"西川拓", age : 33, gender: "男性"　};
+console.log(personalData);
+personalData.age = 34;
+personalData.address ="大阪";
+console.log(personalData);
---
+++ js/11_function.js
@@ -0,0 +1,14 @@
+const sayGoodMorning = () => {
+    console.log("good morning");
+    console.log("What's Up!");
+    console.log("Good Luck!");
+} 
+
+const sayGoodEvening = () => {
+    console.log("good evening");
+    console.log("good job!");
+}
+
+sayGoodMorning();
+
+sayGoodEvening();
